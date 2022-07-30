a6299dbde9f8f1899661b849757f4ceeeaf319b4
sheela7 <sheela7>
2022-07-30 19:18:31 +0545

    apikey added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//7dc9e9be
+
+const API_URL = 'http://www.omdapi.com?apikey=7dc9e9be';
+const App = () => {
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL} &s=${title}`)
+    const data = await response.json();
+
+    console.log(data);
 }
+//useeffect accepts the call back function and an empty dependency array as the 
+//second one if only needed to be called at start reload.
+useEffect(() => {
+    searchMovies('Spiderman')
+}, [])
 
+    return(
+        <h1>App</h1>
+    );
+}
 export default App;
